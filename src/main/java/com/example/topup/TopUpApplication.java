8527ba4e375820354eb3e838b2327658d28f0c8e
1-THANHPHAM2-L\thanh.pham2_onemount <thanh.pham2@onemount.com>
2021-12-06 11:06:34 +0700

    First Commit

---
+++ src/main/java/com/example/topup/TopUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.topup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopUpApplication.class, args);
+    }
+
+}
